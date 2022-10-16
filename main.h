2077994af96581aceef5402f7e5bc6d1f7b4393f
Rabia Hassen <rabia-hassen@hotmail.com>
2022-10-16 17:46:46 +0000

    master

--- main.h
+++ main.h
@@ -2,4 +2,8 @@
 #define MAIN_H
 #include <stdarg.h>
 int _putchar(char c);
+int _printf(const char *format, ...);
+int spec_c(va_list args);
+int _puts(char *str);
+int spec_s(va_list args);
 #endif
